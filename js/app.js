305b4f48b2c44cc436f16d8f34788fbf5c3c4215
kaisapalmer <kaisa.palmer@gmail.com>
2025-10-28 11:54:49 +0100

    content

--- js/app.js
+++ js/app.js
@@ -6,8 +6,15 @@ const searchResult = document.getElementById("results");
 
 /* Creates an event that fetches book data from the Open Library API when the button is clicked
 based on the user's input */
-searchButton.addEventListener('click', async () => {
-  let url ="https://openlibrary.org/search.json?q=" + searchField.value;
+searchButton.addEventListener('click', () => searchBooks());
+searchField.addEventListener('keyup', (e) => {
+  if (e.key === "Enter") {
+    searchBooks();
+  }
+});
+
+async function searchBooks() {
+  let url = "https://openlibrary.org/search.json?q=" + searchField.value;
   const response = await fetch(url);
   const json = await response.json();
   const books = json.docs;
@@ -17,36 +24,43 @@ searchButton.addEventListener('click', async () => {
   for (let i = 0; i < books.length; i++) {
     const book = books[i];
 
-    let bookCard = document.createElement("div");
-    bookCard.className = "book-card";
-
-    let image = document.createElement("img");
-    // om bild finns, plocka då cover_id och konstruera en URL för den,
-    // om bild saknas, så visar vi en "No Photo"-bild som jag hittade på vectorstock.com
-    image.src = book.cover_i
+    const bookCoverUrl = book.cover_i
       ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
       : "https://cdn.vectorstock.com/i/1000v/33/47/no-photo-available-icon-vector-40343347.jpg";
-    bookCard.append(image);
 
-    let bookCardBody = document.createElement("div");
-    bookCardBody.className = "body";
-    bookCard.append(bookCardBody);
+    const author = book.author_name ? book.author_name[0] : "Okänd författare";
+    const cardHtml = `
+      <div class="book-card">
+        <img src="${bookCoverUrl}" alt="book cover">
+        <div class="body">
+          <div class="title">${book.title}</div>
+          <div class="meta">${book.author_name ? book.author_name[0] : "Okänd författare"}</div>
+        </div>
+      </div>
+    `;
 
-    let bookCardTitle = document.createElement("div");
-    bookCardTitle.className = "title";
-    bookCardTitle.innerHTML = book.title;
-    bookCardBody.append(bookCardTitle);
+    const bookCard = document.createElement("div");
+    bookCard.innerHTML = cardHtml;
 
+    bookCard.firstElementChild.addEventListener("click", () => loadBookModal(book, bookCoverUrl, author));
 
-    let bookCardMeta = document.createElement("div");
-    bookCardMeta.className = "meta";
-    bookCardMeta.innerHTML = book.author_name ? book.author_name[0] : "Okänd författare"; // Några böcker har fler författare, ta första för tillfället ..
-    bookCardBody.append(bookCardMeta);
-
-    let bookCardButton = document.createElement("button");
-    bookCardButton.innerHTML = "Köp";
-    //bookCardBody.append(bookCardButton);
-
-    searchResult.append(bookCard);
+    searchResult.append(bookCard.firstElementChild);
   }
-})
+}
+
+async function loadBookModal(book, imageUrl, author) {
+  const detailsUrl = `https://openlibrary.org/books/${book.cover_edition_key}.json`;
+  const response = await fetch(detailsUrl);
+  const detailsJson = await response.json();
+
+  let modal = document.getElementById("bookModal");
+
+  modal.querySelector("#bookCover").src = imageUrl
+  modal.querySelector("#bookTitle").innerHTML = book.title;
+  modal.querySelector("#bookAuthor").innerHTML = author;
+  modal.querySelector("#bookDescription").innerHTML = detailsJson.description ?? "Beskrivning saknas";
+
+  modal.classList.add("show");
+
+  modal.querySelector(".close").addEventListener("click", () => modal.classList.remove("show"));
+}
